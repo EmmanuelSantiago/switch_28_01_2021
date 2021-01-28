8ff112a7a24d3d0167e147a7d68129af9cb3067a
Emmanuel Santiago <biosinx@gmail.com>
2021-01-28 18:59:01 -0500

    switch_28_01_2021

---
+++ main.js
@@ -0,0 +1,27 @@
+let key = parseInt(prompt("Elija un numero"));
+switch (key) {
+    case 1:
+        console.log(`Opcion uno`);        
+        break;
+    case 2:
+        console.log(`Opcion dos`);
+        break;
+    default:
+        console.log(`Opcion final`);
+        break;
+}
+
+
+//function saludar(params) {
+//   return `hola ${params}`;
+//}
+//console.log(saludar('Emmanuel'));
+
+//let saludar_consola = function () {
+//    return function () {
+//    return {valor: 123}        
+//    };
+//};
+
+//let saludar = saludar_consola('hola');
+//console.log(saludar().valor);
